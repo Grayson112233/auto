62be5ef46499b29852c8ec557a2a63a410b302af
Grayson112233 <graysonpike@gmail.com>
2019-11-28 02:18:37 -0600

    Trying to get noinput keygen

--- git_config.sh
+++ git_config.sh
@@ -5,8 +5,11 @@ git config --global user.email "graysonpike@gmail.com"
 
 # Generate a new ssh key if it doesn't already exist
 FILE=~/.ssh/id_rsa.pub
-if test -f "$FILE"; then
-    ssh-keygen -t rsa -b 4096 -C "graysonpike@gmail.com"
+if test -f "$FILE"
+then
+	echo "ssh key already exists"
+else
+    ssh-keygen -t rsa -b 4096 -C "graysonpike@gmail.com" -q -N "" -f ~/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
 fi
--- machine_setup.sh
+++ machine_setup.sh
@@ -1,6 +1,7 @@
 # Script to ensure the machine has the required dependencies and is ready
 # to deploy a Django site
 
+sudo apt-get update
 sudo apt-get install python3 python3-pip python3-venv nginx vim tree git
 bash ./git_config.sh
 bash ./install_dotiles.sh
