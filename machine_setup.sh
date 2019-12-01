d0ceafcd9df9160b8e9f74f2aa962c51c92e1ef8
Grayson112233 <graysonpike@gmail.com>
2019-12-01 15:25:41 -0600

    Added -y flag to apt install

--- machine_setup.sh
+++ machine_setup.sh
@@ -2,7 +2,7 @@
 # to deploy a Django site
 
 sudo apt-get update
-sudo apt-get install python3 python3-pip python3-venv nginx vim tree git
+sudo apt-get install -y python3 python3-pip python3-venv nginx vim tree git
 bash ./git_config.sh
 bash ./install_dotfiles.sh
 sudo ufw allow 'Nginx Full'
