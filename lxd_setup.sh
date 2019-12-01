5c39400028d2dbd8f335870472a16c3188127803
Grayson112233 <graysonpike@gmail.com>
2019-12-01 17:52:48 -0600

    new lxd setup

--- lxd_setup.sh
+++ lxd_setup.sh
@@ -8,9 +8,28 @@ sudo apt install -y lvm2 thin-provisioning-tools
 sudo apt install -y btrfs-tools
 sudo apt install -y curl gettext jq sqlite3 uuid-runtime bzr socat
 
-# Download lxd source
-go get -v -x -d github.com/lxc/lxd/lxc
-go get -v -x -d github.com/lxc/lxd/lxd
+
+go get -v -x -tags libsqlite3 github.com/lxc/lxd/lxc
+go get -v -x -tags libsqlite3 github.com/lxc/lxd/lxd
+
 
 cd ~/go/src/github.com/lxc/lxd
 make deps
+
+read -p "IMPORTANT: Copy the above 3 lines into your ~/.bashrc using a different terminal. This step must be completed before continuing. Press enter to continue."
+
+
+# Create aliases for sudo
+echo "alias sudo='sudo '" >> ~/.bashrc
+echo "alias runlxd='sudo -E LD_LIBRARY_PATH=$LD_LIBRARY_PATH $GOPATH/bin/lxd --group sudo'" >> ~/.bashrc
+echo "alias runlxc='sudo $GOPATH/bin/lxc'" >> ~/.bashrc
+echo "alias delete-lxd-data='sudo pkill -f lxd && sudo pkill -f lxc && sudo rm -rf /var/lib/lxd'" >> ~/.bashrc
+
+# Add our remote and set it
+cd ~/go/src/github.com/lxc/lxd
+git remote add github git@github.com:Grayson112233/lxd.git
+git remote update
+git checkout github/master
+
+# Build our fork
+make
