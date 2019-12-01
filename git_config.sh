01872339dff367b90003bdf56ad786dbddb1f749
Grayson112233 <graysonpike@gmail.com>
2019-12-01 16:17:32 -0600

    Fix git config script

--- git_config.sh
+++ git_config.sh
@@ -9,13 +9,13 @@ if test -f "$FILE"
 then
 	echo "ssh key already exists"
 else
-    ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL" -q -N "" -f "~/.ssh/id_rsa"
+    ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL" -q -N "" -f "$HOME/.ssh/id_rsa"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
 fi
 
 # cat the key for GitHub
-echo "Copy the following key to GitHub:"
+echo "Copy the following key to GitHub as a new SSH key:"
 cat ~/.ssh/id_rsa.pub
 
-read -p "Press enter when you're done."
+read -p "Press enter when you're done. This step must be completed before continuing."
